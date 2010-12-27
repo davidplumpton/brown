7338a1fffae3474cd1a98d20d65600a6cb3a913a
David Plumpton <david.plumpton@gmail.com>
2010-12-27 13:40:57 +1300

    Initial

---
+++ hotkey.js
@@ -0,0 +1,11 @@
+document.addEventListener('keyup', handleHotKey, false);
+
+function handleHotKey(e)
+{
+    // Look for Ctrl-Y
+    if (e.keyCode == 89 && e.ctrlKey && !e.shiftKey && !e.altKey)
+    {
+        var port = chrome.extension.connect({name: "nexty"});
+        port.postMessage({command: "nextPage"});
+    }
+}
